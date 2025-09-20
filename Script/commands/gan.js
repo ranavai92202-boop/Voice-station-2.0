ef079806f27d9e5aad3ca38e38496aa8800648eb
ranavai92202-boop <ranavai92202@gmail.com>
2025-09-21 02:15:08 +0600

    Update gan.js

--- Script/commands/gan.js
+++ Script/commands/gan.js
@@ -15,20 +15,26 @@ module.exports.config = {
 };
 
 const songLinks = [
- "https://drive.google.com/uc?export=download&id=1X_J00k_go_u3MKqKwvZOcypQ-dL6DMAm",
- "https://drive.google.com/uc?export=download&id=1nLq8wKxcxK6nb-8SmJ1nPxNHx9Fzabr8",
- "https://drive.google.com/uc?export=download&id=1w972wKW72haSYHhcIZ_CIpRRv0UAf5TS",
- "https://drive.google.com/uc?export=download&id=1KLAtG03-O7GObVSo7YhkUd84tSTXQOL7",
- "https://drive.google.com/uc?export=download&id=1a3qcxjTi6W6wL4vItVY-SZ7aRpJISpLC",
- "https://drive.google.com/uc?export=download&id=1R2thfTrK3Xk842axn1mPrJ8AdPh8xpLf",
- "https://drive.google.com/uc?export=download&id=1nde8BkUjfD7F5fAM6WvAj6usHGjra4Ln",
- "https://drive.google.com/uc?export=download&id=1JVrIeRhhLUg-qOkRzvZCtI-CGrdfrHvq",
- "https://drive.google.com/uc?export=download&id=1uObNiYcCBbpTNZejRYavBKZGlclD2k3v",
- "https://drive.google.com/uc?export=download&id=1FN1kr3jma9i8opILdeMpH67lHjeJ3NIT",
- "https://drive.google.com/uc?export=download&id=1V2wYr_sGIBckvVrwGmpQXoZ_bj1jR6DY",
- "https://drive.google.com/uc?export=download&id=1FsQbt14Jw7gpvaabkBSgJDCefMLU8Pxq",
- "https://drive.google.com/uc?export=download&id=1ylJsOdaJ53GDITZ6_X-ET5PdnFAW93g1",
- "https://drive.google.com/uc?export=download&id=1Gj7ls2QwDmM-3nN7AXUxPPcGV8hdm59w"
+ "https://drive.google.com/uc?export=download&id=1kmaBUKulLRyrFophTBuULNSm-dTQdwlX",
+ " https://drive.google.com/uc?export=download&id=1E43aZZ-EMmbj1nO0HyC2sMhseUM2JE8L",
+ "https://drive.google.com/uc?export=download&id=1aTjhRMmXbYbAxomts387Bj--c1QBFU-X",
+ "https://drive.google.com/uc?export=download&id=14DT49Bmm8QJqylR67epATwLc3ro4q4CR",
+ "https://drive.google.com/uc?export=download&id=1Ex4viD6y-3uY2jjJUCepWxf_FPPOxFpr",
+ "https://drive.google.com/uc?export=download&id=1a18p8JJaUTNk7jPLVEOOfNRDJc89fWW1",
+ "https://drive.google.com/uc?export=download&id=1RAAGXCRiSXQE7wwkpzKR4xOlHpbSVuBi",
+ "https://drive.google.com/uc?export=download&id=1J-ehtxKugRNfNReTs8xg6Utrva0ewGZk",
+ "https://drive.google.com/uc?export=download&id=16hxyS9gEP2i4HQ983wx_l08VI8-eIO0l",
+ "https://drive.google.com/uc?export=download&id=1Fw_AYfEVEDi8G9E-PIdWAUAwz6E2qE_N",
+ "https://drive.google.com/uc?export=download&id=14uaJC_UEj0NBIXsyQAIdthdVlQVRNMFL",
+ "https://drive.google.com/uc?export=download&id=1F78Y4GA0dv2qV2itPYKiyJ2pD6Qy68cU",
+ "https://drive.google.com/uc?export=download&id=1X_bTzDAbsh7JEBK4VBVyvIdUgdI_0yru",
+ "https://drive.google.com/uc?export=download&id=1g18k7GWQlH-D_8kRcEG7RFTz-RIo0rXH",
+ "https://drive.google.com/uc?export=download&id=1g18k7GWQlH-D_8kRcEG7RFTz-RIo0rXH",
+ "https://drive.google.com/uc?export=download&id=1wJ_ZM8djlQIKbz28-6V61asrsD3S9kfR",
+ "https://drive.google.com/uc?export=download&id=1CBUYRgziBH1I-oesl_zfbDvZFuoc-sUv",
+ "https://drive.google.com/uc?export=download&id=1pFh6DN8SUqTOev5xgX6pQI5ESfmqs5AZ",
+ "https://drive.google.com/uc?export=download&id=1eLoUghjjnN5HSF5-3fQBeUVbfAXTmIK9",
+ "https://drive.google.com/uc?export=download&id=1Tp-0Ao8dI-v8dbr9RN_WNIJRgJg_uTGm"
 ];
 
 module.exports.run = async function ({ api, event, args }) {
